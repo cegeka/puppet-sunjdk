d2724b2c201fbdababb39d93dd8402e35ef71486
fdammeke <fabian.dammekens@cegeka.be>
2012-05-22 17:45:06 +0200

    Added ensure parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,16 +5,22 @@
 # Include it to install the Sun Java6 JDK package.
 #
 # Usage:
-# include sunjdk
+# class { sunjdk:
+#   jdk_version => '',
+#   ensure      => 'present',
+# }
 #
-class sunjdk($jdk_version) {
+class sunjdk($jdk_version,$ensure) {
   if ! $jdk_version {
     $jdk_version = 'latest'
+  } else {
+    $real_jdk_version = $jdk_version
   }
 
   case $::operatingsystem {
     redhat, centos: { include sunjdk::redhat }
     ubuntu: { include sunjdk::ubuntu }
+    windows: { include sunjdk::windows }
     default: {}
   }
 }
