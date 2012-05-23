2150465882cdfec0b662e0a069b6b76ab2195d55
fdammeke <fabian.dammekens@cegeka.be>
2012-05-23 13:35:43 +0200

    Added sample

--- manifests/params.pp
+++
@@ -1,8 +0,0 @@
-class sunjdk::params {
-
-    $version = $operatingsystem ? {
-        redhat => "1.6.0_25",
-        centos => "1.6.0_25",
-        default => "",
-    }
-}
--- tests/init.pp
+++
@@ -1,3 +0,0 @@
-class { 'sunjdk':
-  jdk_version => '1.6.0_27-fcs',
-}
