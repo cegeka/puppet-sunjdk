a6c5620abbcf4f6b970aecc62569abdce5b28c90
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-10-22 17:24:54 +0200

    Several lint fixes + modified ubuntu class to define

--- examples/windows.pp
+++ examples/windows.pp
@@ -1,6 +1,6 @@
 
 sunjdk::instance { 'installing jdk 32bit':
-  ensure         => 'present',
+  ensure          => 'present',
   jdk_version     => '6u32_32b',
   install_options => {
     'INSTALLDIR'  => 'C:\Program Files (x86)\Java\JDK 1.6'
